6e8854ae3d57760e720926fb6770df9ef14edd36
xwaxes01 <xwaxes@gmail.com>
2020-12-07 10:20:27 +0300

    Added opening animations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DeveloperPortfolio2.0
 A new developer portfolio.
+
+Find the portfolio at: https://xwaxes01.github.io/DeveloperPortfolio2.0/
--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ window.onload = function () {
     const curtainLeft = document.getElementById("curtain-left")
     const curtainRight = document.getElementById("curtain-right")
 
-    curtainLeft.classList.add("animated")
-    curtainRight.classList.add("animated")
+    curtainLeft.classList.add("animated-curtain-draw")
+    curtainRight.classList.add("animated-curtain-draw")
+
+    const welcomeText = document.getElementById("welcome-text")
+    welcomeText.classList.add("animated-welcome-text")
+
+    setTimeout(function() {
+        welcomeText.innerHTML = "Welcome to my portfolio"
+        welcomeText.classList.add("animated-welcome-text")
+    }, 9000);
 }
