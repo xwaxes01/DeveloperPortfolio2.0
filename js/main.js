9172111095dc85b02da7c192afdd15bdd11920c1
xwaxes01 <xwaxes@gmail.com>
2020-12-07 09:37:33 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const curtainLeft = document.getElementById("curtain-left")
+    const curtainRight = document.getElementById("curtain-right")
+
+    curtainLeft.classList.add("animated")
+    curtainRight.classList.add("animated")
+}
